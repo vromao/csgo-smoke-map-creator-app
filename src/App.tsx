664cb7afea640f72210346cfa6b7cb91f2d60e41
Vinícius Romão <viniciusromao.contato@gmail.com>
2020-08-29 23:03:25 -0300

    Initial structure for Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
+import Home from './pages/Home';
 import Map from './components/Map/Map';
-import Header from './pages/Header';
 import { DndProvider } from 'react-dnd'
 import Backend from 'react-dnd-html5-backend'
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <DndProvider backend={Backend}>
+      <Home/>
+      {/* <DndProvider backend={Backend}>
         <Map /> 
-			</DndProvider>
+			</DndProvider> */}
     </div>
   );
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React, { Fragment }  from 'react';
+import Header from '../components/Header/Header';
+
+const Home: React.FC = () => {
+  return (
+    <Fragment>
+        <Header />
+    </Fragment>
+  );
+}
+
+export default Home;
